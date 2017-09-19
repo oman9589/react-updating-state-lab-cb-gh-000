9e50ee1e6c80b4403e729161917914bf9467a4ab
Oliver Spring <oliver9589@gmail.com>
2017-09-19 00:15:57 +0000

    automatically backed up by learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {bitrate: 8, video: {resolution: '1080p'}},
+    }
+  }
+}
