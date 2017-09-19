025cc449c6156f15677f9a74cb8ac60f0e137d45
Oliver Spring <oliver9589@gmail.com>
2017-09-19 00:12:08 +0000

    automatically backed up by learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -3,9 +3,21 @@ import React from 'react';
 class DigitalClicker extends React.Component {
   constructor() {
     super()
-    
+
     this.state =  {
       timesClicked: 0,
     }
+  }
+
+  handleClick = () => {
+    this.setState({
+      timesClicked: this.timesClicked+1;
+    })
+  }
+
+  render() {
+    return (
+      <button
+    );
   }
 }
